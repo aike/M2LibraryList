6f889391aa1775b5b5c1aefe31dd1401ea00c937
aike <aaiikkee@gmail.com>
2023-12-27 14:41:32 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+M2LibraryList
+---
+A script that generates a comparison chart between the Maschine 2 Full Factory Library and the Factory Selection.
+
+## Webpage
+https://aike.github.io/m2librarylist/
+
+![screenshot](ss.png)
+
+## Description
+  
+There are several types of Maschine 2 libraries.
+
+- Factory Selection (1.6 GB)
+- Full Factory Library (8 GB)
+- Expansions
+  
+[Maschine Essentials](https://www.native-instruments.com/en/products/maschine/maschine-accessories/maschine-essentials/) is the Maschine software bundled with hardware and includes ***Factory Selection***.  
+  
+Maschine Essentials owners can [upgrade](https://www.native-instruments.com/en/products/maschine/maschine-accessories/upgrade-your-library/) their ***Factory Selection*** to the ***Full Factory Library*** for a fee.  
+  
+Expansions are additional libraries for purchase and do not contain the content of ***Full Factory Library*** / ***Factory Selection***.  
+  
+[KOMPLETE](https://www.native-instruments.com/en/products/komplete/bundles/komplete-14-standard/compare/) includes several Expansions, but they do not contain the content of ***Full Factory Library*** / ***Factory Selection***.  
+  
+There is no official content list of the libraries.
+So I make a script that generates a comparison chart between the ***Full Factory Library*** and the ***Factory Selection***.
+
+## License
+M2LibraryList program is licensed under MIT License.  
+Copyright 2023, aike (@aike1000)  
---
+++ makehtml.py
@@ -0,0 +1,97 @@
+import os
+import glob
+import datetime
+
+dir1 = "F:/dtm/NI/Maschine 2 Factory Selection/"
+dir2 = "F:/dtm/NI/Maschine 2 Factory Library/"
+
+def dirdiff(dir1, dir2):
+
+  file1 = glob.glob(dir1 + '**/*', recursive=True)
+  file2 = glob.glob(dir2 + '**/*', recursive=True)
+
+  file1_noroot = sorted(list(map(lambda x: x.replace('\\','/').replace(dir1, ""), file1)), key=str.casefold)
+  file2_noroot = sorted(list(map(lambda x: x.replace('\\','/').replace(dir2, ""), file2)), key=str.casefold)
+
+  # "Bass Synth"フォルダと "Bass"フォルダの子要素を正しくソートできないので、末尾にスラッシュを追加
+  file3 = list(set(file1_noroot + file2_noroot))
+  for i in range(len(file3)):
+    if (os.path.isdir(dir1 + file3[i])) or (os.path.isdir(dir2 + file3[i])):
+      file3[i] = file3[i] + "/"
+
+  file_all = sorted(file3, key=str.casefold)
+
+  dirno = 0
+  dirs = {}
+  cnt_sel = 0
+  cnt_ful = 0
+
+  for f in file_all:
+    if f.startswith("Documentation"):
+      continue
+
+    clsstr = ""
+    isdir = False
+    if (os.path.isdir(dir1 + f) or os.path.isdir(dir2 + f)):
+      f = f[:-1]
+      isdir = True
+      dirno += 1
+      clsstr = "p" + str(dirno)
+      dirs[f] = dirno
+
+    fname = os.path.basename(f)
+    d = os.path.dirname(f)
+    l = 0
+    while (d != ""):
+      l += 1
+      if clsstr != "":
+        clsstr = clsstr + " "
+      clsstr = clsstr + "c" + str(dirs[d])
+      d = os.path.dirname(d)
+    clsstr = clsstr + " l" + str(l)
+    if (os.path.dirname(f) != ""):
+      clsstr = clsstr + " h"
+
+    if isdir:
+      clsstr = clsstr + " d"
+ 
+    print('<tr class="' + clsstr + '"><td>', end='')
+    if f in file1_noroot:
+      print(fname, end='')
+    print("</td><td>", end='')
+    if f in file2_noroot:
+      print(fname, end='')
+    print("</td></tr>")
+
+    if not isdir:
+      if f in file1_noroot:
+        cnt_sel += 1
+      if f in file2_noroot:
+        cnt_ful += 1
+
+  return cnt_sel, cnt_ful
+
+print("<html>")
+print("<head>")
+print("<link rel=Stylesheet href=style.css>")
+print("</head>")
+print("<body>")
+print('<div id="title"><h1>Maschine 2 Factory Selection vs Full Maschine Library</h1></div>')
+print('<div id="control"><div id="collapse">Collapse All</div><div id="expand">Expand All</div>')
+print('<input type="search" id="search" placeholder="Search..."></div>')
+print("<table>")
+print("<tr>")
+print('<th>Factory Selection<span id="cnt_selection">(0000 items)</span></th>')
+print('<th >Full Maschine Library<span id="cnt_full">(00000 items)</span></th>')
+print("</tr>")
+
+cnt_sel, cnt_ful = dirdiff(dir1, dir2)
+
+print("</table>")
+print('<div id="update">{} update</div>'.format(str(datetime.datetime.today().date())))
+print('<div id="about"><a href="https://github.com/aike/m2librarylist">about</a></div>')
+print('<div id="cnt_selection_value" value="{}" style="display:none"/>'.format(cnt_sel))
+print('<div id="cnt_full_value" value="{}" style="display:none"/>'.format(cnt_ful))
+print('<script src="script.js"></script>')
+print("</body>")
+print("</html>")
